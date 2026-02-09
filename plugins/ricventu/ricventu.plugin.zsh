7731bf6a09ab006787dafa3dbb47a6dbcc83a1e3
Riccardo Venturini <riccardo@fornacestudio.com>
2026-02-09 14:38:54 +0100

    Update ricventu.plugin.zsh to enhance PATH configuration and add new aliases

--- plugins/ricventu/ricventu.plugin.zsh
+++ plugins/ricventu/ricventu.plugin.zsh
@@ -1,20 +1,30 @@
 export PATH="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/utils:$PATH"
 
 export NPM_PACKAGES="${HOME}/.npm-packages"
-export PATH="$NPM_PACKAGES/bin:/usr/local/bin:$PATH"
+export PATH="${HOME}/.composer/vendor/bin:$NPM_PACKAGES/bin:/usr/local/bin:$PATH"
 export EDITOR=cursor
 export NEXT_EDITOR=cursor
 export COMPOSER_MEMORY_LIMIT=-1
 
 # HOMEBREW
+
 # brew install fzf bat eza zoxide
 export HOMEBREW_PREFIX="/opt/homebrew";
 export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
 export HOMEBREW_REPOSITORY="/opt/homebrew";
-export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
-export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
+fpath[1,0]="/opt/homebrew/share/zsh/site-functions";
+eval "$(/usr/bin/env PATH_HELPER_ROOT="/opt/homebrew" /usr/libexec/path_helper -s)"
+[ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}";
 export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
 export PATH="/opt/homebrew/opt/mysql-client@8.0/bin:$PATH";
+export PATH="/opt/homebrew/opt/php@8.4/bin:$PATH";
+export PATH="/opt/homebrew/opt/php@8.4/sbin:$PATH";
+export PATH="/opt/homebrew/opt/postgresql@18/bin:$PATH"
+
+# Cursor Agent
+export PATH="$HOME/.local/bin:$PATH"
 
 # NVM
 export NVM_DIR="$HOME/.nvm"
@@ -36,7 +46,14 @@ alias zshrc='code ~/.zshrc "$ZSH" "$ZSH_CUSTOM"'
 alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
 alias xphp='XDEBUG_MODE=debug XDEBUG_TRIGGER=1 IDEKEY=PHPSTORM php'
 alias docker-stop-all='dsta'
-
+alias a='php artisan'
+alias xa='xphp artisan'
+alias artisan='php artisan'
+alias xartisan='xphp artisan'
+alias phpstan='[ -f phpstan ] && sh phpstan || sh vendor/bin/phpstan'
+alias c='composer'
+alias xc='xphp /opt/homebrew/bin/composer'
+alias xcomposer='xphp /opt/homebrew/bin/composer'
 
 function laravelsail() {
     docker run --rm --interactive --tty \
